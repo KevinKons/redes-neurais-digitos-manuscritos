93e11dfe4b36d3deb0e4608ff21819d16ff8759a
Douglas <martins.douglas.souza@gmail.com>
2019-05-01 16:46:50 -0300

    Auto stash before revert of "Alterado o gitigonore"

--- DigitosManuscritos/src/Main.java
+++ DigitosManuscritos/src/Main.java
@@ -1,10 +1,14 @@
+import org.encog.engine.network.activation.ActivationLOG;
+import org.encog.ml.model.config.FeedforwardConfig;
 import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("oi");
 
-
+        BasicNetwork network = new BasicNetwork();
+        network.addLayer(new BasicLayer(new ActivationLOG(), true, ));
     }
 }
