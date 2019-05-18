d13a089d4e7dda9f7de8111406c166538ef9d6c7
Douglas <martins.douglas.souza@gmail.com>
2019-05-18 16:14:56 -0300

    Implementada a classe de RedeNeural para a rede aprender

--- DigitosManuscritos/src/rns/RedeNeural.java
+++ DigitosManuscritos/src/rns/RedeNeural.java
@@ -1,21 +1,46 @@
 package rns;
 
+import org.encog.Encog;
 import org.encog.engine.network.activation.ActivationTANH;
+import org.encog.ml.data.MLData;
 import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLData;
+import org.encog.ml.data.basic.BasicMLDataSet;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.propagation.back.Backpropagation;
 
 public class RedeNeural {
 
-    public void aprender(int entrada[][], int saida[][]){
+    public void aprender(double entrada[][], double saida[][]) {
 
         BasicNetwork network = new BasicNetwork();
-        network.addLayer(new BasicLayer(null,true,2));
-        network.addLayer(new BasicLayer(new ActivationTANH(),true,3));
-        network.addLayer(new BasicLayer(new ActivationTANH(),false,1));
+        network.addLayer(new BasicLayer(null, true, 2));
+        network.addLayer(new BasicLayer(new ActivationTANH(), true, 3));
+        network.addLayer(new BasicLayer(new ActivationTANH(), false, 1));
         network.getStructure().finalizeStructure();
         network.reset();
-//        MLDataSet dadosTreinamento = new BasicMLDataSet()
+        MLDataSet dadosTreinamento = new BasicMLDataSet(entrada, saida);
+        final Backpropagation treinamento = new Backpropagation(network, dadosTreinamento);
+        System.out.println("Treinando a RN...");
+        int contadorEpocas = 1;
+
+        do {
+            treinamento.iteration();
+            System.out.println("Época #" + contadorEpocas + " Erro:" + treinamento.getError());
+            contadorEpocas++;
+        } while (treinamento.getError() > 0.01);
+
+        treinamento.finishTraining();
+        System.out.println("Testando a RN com as entradas...");
+
+        for (int i = 0; i < entrada[0].length; i++) {
+            MLData caso1Entrada = new BasicMLData(entrada[i]);
+            MLData saidaCaso1 = network.compute(caso1Entrada);
+            System.out.println("Entrada caso" + i + ": " + caso1Entrada + " --> Saída: " + saidaCaso1);
+        }
+
+        Encog.getInstance().shutdown();
 
     }
 
