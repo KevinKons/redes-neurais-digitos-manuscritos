7c6437131e261253815c73390610e32ffb59e968
Douglas <martins.douglas.souza@gmail.com>
2019-05-18 15:42:58 -0300

    Adicionado o .jar do java encog

--- DigitosManuscritos/src/rns/RedeNeural.java
+++ DigitosManuscritos/src/rns/RedeNeural.java
@@ -1,21 +1,21 @@
 package rns;
 
 import org.encog.engine.network.activation.ActivationTANH;
+import org.encog.ml.data.MLDataSet;
 import org.encog.neural.networks.BasicNetwork;
 import org.encog.neural.networks.layers.BasicLayer;
 
 public class RedeNeural {
 
-    public void aprender(){
+    public void aprender(int entrada[][], int saida[][]){
 
-    BasicNetwork network = new BasicNetwork();
-    network.addLayer(new BasicLayer(null,true,2));
-
-    network.addLayer(new BasicLayer(new ActivationTANH(),true,3)); // camada oculta
-
-    network.addLayer(new BasicLayer(new ActivationTANH(),false,1)); // camada de sa�da
-
-    network.getStructure().finalizeStructure(); // finaliza a estrutura da rede
+        BasicNetwork network = new BasicNetwork();
+        network.addLayer(new BasicLayer(null,true,2));
+        network.addLayer(new BasicLayer(new ActivationTANH(),true,3));
+        network.addLayer(new BasicLayer(new ActivationTANH(),false,1));
+        network.getStructure().finalizeStructure();
+        network.reset();
+//        MLDataSet dadosTreinamento = new BasicMLDataSet()
 
     }
 
