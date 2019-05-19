5bf92a085b6ad4bed92a2b47f17d6c8d336a6dd8
KevinKons <kevin.kons@hotmail.com>
2019-05-19 19:08:15 -0300

    Calculo de Precisao, sensibilidade, acurácia e f1-score

--- DigitosManuscritos/src/controller/ControllerRedeNeural.java
+++ DigitosManuscritos/src/controller/ControllerRedeNeural.java
@@ -2,35 +2,38 @@ package controller;
 
 import rns.RedeNeural;
 import util.LeitorDataset;
+import util.OperacaoComMatriz;
 
 public class ControllerRedeNeural {
 
+    private int tamanhoFold;
+
     @SuppressWarnings("Duplicates")
     public void montarEstruturaCrossValidation(int folds) {
-        double[][] entradas = new double[RedeNeural.quantidadeExemplos][RedeNeural.quantidadePixels];
-        double[][] saidas  = new double[RedeNeural.quantidadeExemplos][10];
+        double[][] entradas = new double[RedeNeural.qntExemplos][RedeNeural.qntPixels];
+        double[][] saidas  = new double[RedeNeural.qntExemplos][10];
 
         LeitorDataset leitorDataset = LeitorDataset.getInstance();
         leitorDataset.lerDataset(entradas, saidas,"semeion.data");
 
-        int tamanhoFold = (int) Math.floor(RedeNeural.quantidadeExemplos / folds);
-        for(int i = 0; i < folds; i++) {
-            treinarApartirDeModelo(i, tamanhoFold, entradas, saidas);
-        }
+        tamanhoFold = RedeNeural.qntExemplos / folds;
+        for(int i = 0; i < folds; i++)
+            treinarApartirDeModelo(i, entradas, saidas);
     }
 
-    private void treinarApartirDeModelo(int numeroModelo, int tamanhoFold, double[][] todasEntradas, double[][] todasSaidas) {
-        double[][] entradasParaTeste = new double[tamanhoFold][RedeNeural.quantidadePixels];
+    private void treinarApartirDeModelo(int numeroModelo,
+                                        double[][] todasEntradas, double[][] todasSaidas) {
+        double[][] entradasParaTeste = new double[tamanhoFold][RedeNeural.qntPixels];
         double[][] saidasParaTeste  = new double[tamanhoFold][10];
 
-        double[][] entradasParaConstrucao = new double[RedeNeural.quantidadeExemplos - tamanhoFold][RedeNeural.quantidadePixels];
-        double[][] saidasParaConstrucao = new double[RedeNeural.quantidadeExemplos - tamanhoFold][10];
+        double[][] entradasParaConstrucao = new double[RedeNeural.qntExemplos - tamanhoFold][RedeNeural.qntPixels];
+        double[][] saidasParaConstrucao = new double[RedeNeural.qntExemplos - tamanhoFold][10];
 
         int menorPosicaoVaziaTeste = 0;
         int menorPosicaoVaziaConstrucao = 0;
 
         int primeiraPosParaTeste = tamanhoFold * numeroModelo;
-        for(int i = 0; i < todasEntradas.length; i++) {
+        for(int i = 0; i < todasEntradas.length; i++)
             if(i >= primeiraPosParaTeste && i < primeiraPosParaTeste + tamanhoFold) {
                 entradasParaTeste[menorPosicaoVaziaTeste] = todasEntradas[i];
                 saidasParaTeste[menorPosicaoVaziaTeste] = todasSaidas[i];
@@ -40,10 +43,125 @@ public class ControllerRedeNeural {
                 saidasParaConstrucao[menorPosicaoVaziaConstrucao] = todasSaidas[i];
                 menorPosicaoVaziaConstrucao++;
             }
-        }
 
         RedeNeural rn = new RedeNeural();
-        rn.aprender(entradasParaConstrucao, saidasParaConstrucao, entradasParaTeste, saidasParaTeste, numeroModelo);
+        double[][] matrizSaida = rn.aprender(entradasParaConstrucao, saidasParaConstrucao, entradasParaTeste,
+                saidasParaTeste, numeroModelo);
 
+        int[] vetorSaidaPrevista = new int[matrizSaida.length];
+        int[] vetorSaidaReal = new int[matrizSaida.length];
+        for(int i = 0; i < matrizSaida.length; i++) {
+            vetorSaidaPrevista[i] = retornaSaida(matrizSaida[i]);
+            vetorSaidaReal[i] = retornaSaida(saidasParaTeste [i]);
+        }
+
+        LeitorDataset.getInstance().escreverSaidas(vetorSaidaReal, vetorSaidaPrevista);
+
+        int[][] matrizConfusao = geraMatrizConfusao(vetorSaidaReal, vetorSaidaPrevista);
+        double acuracia = calculaAcuracia(matrizConfusao);
+        double[] precisao = calculaPresicao(matrizConfusao);
+        double[] sensibilidade = calculaSensibilidade(matrizConfusao);
+        double precisaoMedia = calculaPrecisaoMedia(precisao);
+        double sensibilidadeMedia = calculaSensibilidadeMedia(precisao);
+
+        System.out.println( "Acuracia " + acuracia);
+        System.out.println("Precisao Media: " + precisaoMedia);
+        System.out.println("Precisao");
+        for(int i = 0; i < RedeNeural.qntClasses; i++) {
+            System.out.print(precisao[i] + "\t");
+        }
+
+        System.out.println("\n" + "Sensiblidade Media: " + sensibilidadeMedia);
+        System.out.println("\n" + "Sensibilidades:");
+        for(int i = 0; i < RedeNeural.qntClasses; i++) {
+            System.out.print(sensibilidade[i] + "\t");
+        }
+
+        System.out.println("\n" + "F1-Score");
+        System.out.println(2 * ((precisaoMedia * sensibilidadeMedia) / (precisaoMedia + sensibilidadeMedia)));
+    }
+
+    @SuppressWarnings("Duplicates")
+    private double calculaPrecisaoMedia(double[] precisao) {
+        double somaPrecisoes = 0;
+        for(int i = 0; i < RedeNeural.qntClasses; i++)
+            somaPrecisoes += precisao[i];
+        return somaPrecisoes / RedeNeural.qntClasses;
+    }
+
+    @SuppressWarnings("Duplicates")
+    private double calculaSensibilidadeMedia(double[] precisao) {
+        double somaSensibilidade = 0;
+        for(int i = 0; i < RedeNeural.qntClasses; i++)
+            somaSensibilidade += precisao[i];
+        return somaSensibilidade / RedeNeural.qntClasses;
+    }
+
+    @SuppressWarnings("Duplicates")
+    private double[] calculaSensibilidade(int[][] matrizConfusao) {
+        int[] vetorFalsoNegativo = new int[RedeNeural.qntClasses];
+        double[] vetorVerdadeiroPositivo = new double[RedeNeural.qntClasses];
+        for(int i = 0; i < RedeNeural.qntClasses; i++)
+            for(int j = 0; j < RedeNeural.qntClasses; j++)
+                if(i == j)
+                    vetorVerdadeiroPositivo[i] = matrizConfusao[i][j];
+                else
+                    vetorFalsoNegativo[i] += matrizConfusao[i][j];
+
+        double[] sensibilidades = new double[RedeNeural.qntClasses];
+        for(int i = 0; i < RedeNeural.qntClasses; i++)
+            sensibilidades[i] = vetorVerdadeiroPositivo[i] / (vetorVerdadeiroPositivo[i] + vetorFalsoNegativo[i]);
+        return sensibilidades;
+    }
+
+    @SuppressWarnings("Duplicates")
+    private double[] calculaPresicao(int[][] matrizConfusao) {
+        matrizConfusao = OperacaoComMatriz.calculaMatrizTransposta(matrizConfusao);
+        int[] vetorFalsoPositivo = new int[RedeNeural.qntClasses];
+        double[] vetorVerdadeiroPositivo = new double[RedeNeural.qntClasses];
+        for(int i = 0; i < RedeNeural.qntClasses; i++)
+            for(int j = 0; j < RedeNeural.qntClasses; j++)
+                if(i == j)
+                    vetorVerdadeiroPositivo[i] = matrizConfusao[i][j];
+                else
+                    vetorFalsoPositivo[i] += matrizConfusao[i][j];
+
+        double[] precisoes = new double[RedeNeural.qntClasses];
+        for(int i = 0; i < RedeNeural.qntClasses; i++)
+            precisoes[i] = vetorVerdadeiroPositivo[i] / (vetorVerdadeiroPositivo[i] + vetorFalsoPositivo[i]);
+        return precisoes;
+    }
+
+    private double calculaAcuracia(int[][] matrizConfusao) {
+        int vp = calculaVerdadeiroPositivo(matrizConfusao);
+        double tamanhoFold = this.tamanhoFold;
+        return vp / tamanhoFold;
+    }
+
+    private int calculaVerdadeiroPositivo(int[][] matrizConfusao) {
+        int vp = 0;
+        for(int i = 0; i < RedeNeural.qntClasses; i++)
+            vp += matrizConfusao[i][i];
+        return vp;
+    }
+
+    private int[][] geraMatrizConfusao(int[] vetorSaidaReal, int[] vetorSaidaPrevista) {
+        int[][] matrizConfusao = new int[RedeNeural.qntClasses][RedeNeural.qntClasses];
+        for(int i = 0; i < tamanhoFold; i++)
+            matrizConfusao[vetorSaidaPrevista[i]][vetorSaidaReal[i]]++;
+        return matrizConfusao;
+    }
+
+
+
+    private int retornaSaida(double[] saida) {
+        int numeroAtual = 0;
+        double maiorPorcentagem = 0;
+        for(int i = 0; i < saida.length; i++)
+            if(saida[i] > maiorPorcentagem) {
+                maiorPorcentagem = saida[i];
+                numeroAtual = i;
+            }
+        return numeroAtual;
     }
 }
--- DigitosManuscritos/src/rns/RedeNeural.java
+++ DigitosManuscritos/src/rns/RedeNeural.java
@@ -2,7 +2,6 @@ package rns;
 
 import org.encog.Encog;
 import org.encog.engine.network.activation.ActivationSigmoid;
-import org.encog.engine.network.activation.ActivationTANH;
 import org.encog.ml.data.MLData;
 import org.encog.ml.data.MLDataSet;
 import org.encog.ml.data.basic.BasicMLData;
@@ -14,21 +13,23 @@ import util.LeitorDataset;
 
 public class RedeNeural {
 
-    public static final int quantidadePixels = 256;
-    public static final int quantidadeExemplos = 1593;
+    public static final int qntClasses = 10;
+    public static final int qntPixels = 256;
+    public static final int qntExemplos = 1593;
 
     @SuppressWarnings("Duplicates")
-    public void aprender(double[][] entradasParaConstrucao, double[][] saidasParaConstrucao,
-                         double[][] entradasParaTeste, double[][] saidasParaTeste,
-                         int numeroModelo) {
+    public double[][] aprender(double[][] entradasParaConstrucao, double[][] saidasParaConstrucao,
+                               double[][] entradasParaTeste, double[][] saidasParaTeste,
+                               int numeroModelo) {
 
         BasicNetwork network = new BasicNetwork();
 
-        network.addLayer(new BasicLayer(null, true, quantidadePixels));
+        network.addLayer(new BasicLayer(null, true, qntPixels));
+        network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 60));
         network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 50));
         network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 50));
-        network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 25));
-        network.addLayer(new BasicLayer(new ActivationSigmoid(), false, 10));
+        network.addLayer(new BasicLayer(new ActivationSigmoid(), true, 80));
+        network.addLayer(new BasicLayer(new ActivationSigmoid(), false, qntClasses));
 
         network.getStructure().finalizeStructure();
         network.reset();
@@ -51,14 +52,17 @@ public class RedeNeural {
         String entradaSaida = "";
         System.out.println("Testando a RN com as entradas...");
 
+        double[][] saidaDaRede = new double[saidasParaTeste.length][saidasParaTeste[0].length];
         for (int i = 0; i < entradasParaTeste.length; i++) {
-            MLData caso1Entrada = new BasicMLData(entradasParaTeste[i]);
-            MLData saidaCaso1 = network.compute(caso1Entrada);
-            System.out.println("Entrada caso" + i + ": " + caso1Entrada + " --> Saída: " + saidaCaso1);
-            entradaSaida += "Entrada caso" + i + ": " + caso1Entrada + " --> Saída: " + saidaCaso1 + "\n";
+            MLData casoEntrada = new BasicMLData(entradasParaTeste[i]);
+            MLData saidaCaso = network.compute(casoEntrada);
+            saidaDaRede[i] = saidaCaso.getData();
+            System.out.println("Entrada caso" + i + ": " + casoEntrada + " --> Saída: " + saidaCaso);
+            entradaSaida += "Entrada caso" + i + ": " + casoEntrada + " --> Saída: " + saidaCaso + "\n";
         }
         Encog.getInstance().shutdown();
         LeitorDataset.getInstance().escreverSaida(epocaError, entradaSaida, numeroModelo);
+        return saidaDaRede;
     }
 
 }
